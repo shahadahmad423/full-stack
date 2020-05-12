373dd6723c8e96419f84eea1f5bbf51f344b1a94
shahad <shahadahmad423@gmail.com>
2020-05-12 20:56:42 +0300

    first

--- app1.py
+++ app1.py
@@ -15,12 +15,15 @@ app1.debug=True
 db = SQLAlchemy(app1)
 
 
+
+
 class Artist(db.Model):
     __tablename__ = 'Artist'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    email = db.Column(db.String(120), unique=True)
     city = db.Column(db.String(120))
-    state= db.Column(db.String(120))
+    state = db.Column(db.String(120))
     phone = db.Column(db.String(120), unique=True)
     genres = db.Column(db.String(120))
     facbook_link = db.Column(db.String(120), unique=True)
@@ -28,33 +31,27 @@ class Artist(db.Model):
     seeking__description = db.Column(db.String(500))
     shows = db.relationship('Show', backref='Artist', lazy=True)
 
-
     def __repr__(self):
-        return '<Artist %r>' % self.name , self.city , self.state , self.phone ,self.photo, self.genres , self.facbook_link , self.seeking__description
-    
-    def __init__(self , name , city,state , phone , genres, facbook_link,seeking__description):
-          self.name =name
-          self.city=city
-          self.state=state
-          self.phone=phone
-          self.genres=genres
-          self.facbook_link=facbook_link
-          self.seeking__description=seeking__description
-          self.photo=photo
+        return f'Artist ID:{self.id}, , Name: {self.name} , City: {self.city} ,State: {self.state}, Phone: {self.phone}, Genres: {self.genres}, Facbook_link: {self.facbook_link}, Poto: {self.photo}, Seeking__description: {self.seeking__description} '
+
+
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     city = db.Column(db.String(120))
-    address = db.Column(db.String(500))
+    state = db.Column(db.String(120))
     phone = db.Column(db.String(120), unique=True)
     genres = db.Column(db.String(120))
     facbook_link = db.Column(db.String(120), unique=True)
     photo = db.Column(db.String(120),default='default.jpg')
     shows=db.relationship('Show',backref='owner')
 
+ 
 
+    def __repr__(self):
+        return f'Venue ID:{self.id}, , Name: {self.name} , City: {self.city} ,State: {self.state}, Phone: {self.phone}, Genres: {self.genres}, Facbook_link: {self.facbook_link}, Poto: {self.photo} '
 
 
 
@@ -68,11 +65,14 @@ class Show(db.Model):
 
    
     def __repr__(self):
-        return f'<Show ID:{self.id}, start_time: {self.start_time}>'
+        return f'Show ID:{self.id}, start_time: {self.start_time}'
+
+
+    
 
     @app1.route("/post_Artist", methods=['POST'])
     def Artist_create():
-       artist = Artist(request.form['name'], request.form['city'], request.form['state'], request.form['phone'], request.form['genres'], request.form['facbook_link'], request.form['seeking__description'], )
+       artist = Artist(request.form['name'], request.form['city'], request.form['state'], request.form['phone'], request.form['genres'], request.form['facbook_link'], request.form['seeking__description'])
        db.session.add(artist)
        db.session.commit()
        return redirect(url_for(index))
